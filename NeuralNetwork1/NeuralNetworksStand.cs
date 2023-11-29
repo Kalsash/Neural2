8d32a99b86f2e695d7b45b2830fc11bd82a335e6
Aleksandr Kalinin <akalinin@sfedu.ru>
2023-11-29 07:41:12 +0300

    Добавьте файлы проекта.

---
+++ NeuralNetwork1/NeuralNetworksStand.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralNetwork1
+{
+    public partial class NeuralNetworksStand : Form
+    {
+        /// <summary>
+        /// Генератор изображений (образов)
+        /// </summary>
+        //GenerateImage generator = new GenerateImage();
+        private DatasetProcessor dataset = new DatasetProcessor();
+
+        /// <summary>
+        /// Текущая выбранная через селектор нейросеть
+        /// </summary>
+        public BaseNetwork Net
+        {
+            get
+            {
+                var selectedItem = (string) netTypeBox.SelectedItem;
+                if (!networksCache.ContainsKey(selectedItem))
+                    networksCache.Add(selectedItem, CreateNetwork(selectedItem));
+
+                return networksCache[selectedItem];
+            }
+        }
+
+        private readonly Dictionary<string, Func<int[], BaseNetwork>> networksFabric;
+        private Dictionary<string, BaseNetwork> networksCache = new Dictionary<string, BaseNetwork>();
+
+        /// <summary>
+        /// Конструктор формы стенда для работы с сетями
+        /// </summary>
+        /// <param name="networksFabric">Словарь функций, создающих сети с заданной структурой</param>
+        public NeuralNetworksStand(Dictionary<string, Func<int[], BaseNetwork>> networksFabric)
+        {
+            InitializeComponent();
+            this.networksFabric = networksFabric;
+            netTypeBox.Items.AddRange(this.networksFabric.Keys.Select(s => (object) s).ToArray());
+            netTypeBox.SelectedIndex = 0;
+            dataset.LetterCount = (int) classCounter.Value;
+            button3_Click(this, null);
+            pictureBox1.Image = Properties.Resources.Title;
+        }
+
+        public void UpdateLearningInfo(double progress, double error, TimeSpan elapsedTime)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                progressBar1.Invoke(new TrainProgressHandler(UpdateLearningInfo), progress, error, elapsedTime);
+                return;
+            }
+
+            StatusLabel.Text = "Ошибка: " + error;
+            int progressPercent = (int) Math.Round(progress * 100);
+            progressPercent = Math.Min(100, Math.Max(0, progressPercent));
+            elapsedTimeLabel.Text = "Затраченное время : " + elapsedTime.Duration().ToString(@"hh\:mm\:ss\:ff");
+            progressBar1.Value = progressPercent;
+        }
+        
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            var fullSample = dataset.getSample();
+
+            Net.Predict(fullSample.Item1);
+            
+            label1.ForeColor = fullSample.Item1.Correct() ? Color.Green : Color.Red;
+
+            label1.Text = "Распознано : " + DatasetProcessor.LetterTypeToString(fullSample.Item1.recognizedClass);
+
+            //label8.Text = string.Join("\n", figure.Output.Select(d => d.ToString(CultureInfo.InvariantCulture)));
+            pictureBox1.Image = fullSample.Item2;
+            pictureBox1.Invalidate();
+        }
+
+        private async Task<double> train_networkAsync(int training_size, int epoches, double acceptable_error,
+            bool parallel = true)
+        {
+            //  Выключаем всё ненужное
+            label1.Text = "Выполняется обучение...";
+            label1.ForeColor = Color.Red;
+            groupBox1.Enabled = false;
+            pictureBox1.Enabled = false;
+
+            //  Создаём новую обучающую выборку
+            SamplesSet samples = dataset.getTrainDataset(training_size);
+            
+            try
+            {
+                //  Обучение запускаем асинхронно, чтобы не блокировать форму
+                var curNet = Net;
+                double f = await Task.Run(() => curNet.TrainOnDataSet(samples, epoches, acceptable_error, parallel));
+
+                label1.Text = "Щелкните на картинку для теста нового образа";
+                label1.ForeColor = Color.Green;
+                groupBox1.Enabled = true;
+                pictureBox1.Enabled = true;
+                StatusLabel.Text = "Ошибка: " + f;
+                StatusLabel.ForeColor = Color.Green;
+                return f;
+            }
+            catch (Exception e)
+            {
+                label1.Text = $"Исключение: {e.Message}";
+            }
+
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            train_networkAsync((int) TrainingSizeCounter.Value, (int) EpochesCounter.Value,
+                (100 - AccuracyCounter.Value) / 100.0, false);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Enabled = false;
+            //  Тут просто тестирование новой выборки
+            //  Создаём новую обучающую выборку
+            SamplesSet samples = dataset.getTestDataset((int) TrainingSizeCounter.Value);
+
+            double accuracy = samples.TestNeuralNetwork(Net);
+
+            StatusLabel.Text = $"Точность на тестовой выборке : {accuracy * 100,5:F2}%";
+            StatusLabel.ForeColor = accuracy * 100 >= AccuracyCounter.Value ? Color.Green : Color.Red;
+
+            Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //  Проверяем корректность задания структуры сети
+            int[] structure = CurrentNetworkStructure();
+            if (structure.Length < 2 || structure[0] != 200 ||
+                structure[structure.Length - 1] != dataset.LetterCount)
+            {
+                MessageBox.Show(
+                    $"В сети должно быть более двух слоёв, первый слой должен содержать 200 нейронов, последний - ${dataset.LetterCount}",
+                    "Ошибка", MessageBoxButtons.OK);
+                return;
+            }
+
+            // Чистим старые подписки сетей
+            foreach (var network in networksCache.Values)
+                network.TrainProgress -= UpdateLearningInfo;
+            // Пересоздаём все сети с новой структурой
+            networksCache = networksCache.ToDictionary(oldNet => oldNet.Key, oldNet => CreateNetwork(oldNet.Key));
+        }
+
+        private int[] CurrentNetworkStructure()
+        {
+            return netStructureBox.Text.Split(';').Select(int.Parse).ToArray();
+        }
+
+        private void classCounter_ValueChanged(object sender, EventArgs e)
+        {
+            dataset.LetterCount = (int) classCounter.Value;
+            var vals = netStructureBox.Text.Split(';');
+            if (!int.TryParse(vals.Last(), out _)) return;
+            vals[vals.Length - 1] = classCounter.Value.ToString();
+            netStructureBox.Text = vals.Aggregate((partialPhrase, word) => $"{partialPhrase};{word}");
+        }
+        
+        private BaseNetwork CreateNetwork(string networkName)
+        {
+            var network = networksFabric[networkName](CurrentNetworkStructure());
+            network.TrainProgress += UpdateLearningInfo;
+            return network;
+        }
+
+        private void recreateNetButton_MouseEnter(object sender, EventArgs e)
+        {
+            infoStatusLabel.Text = "Заново пересоздаёт сеть с указанными параметрами";
+        }
+
+        private void netTrainButton_MouseEnter(object sender, EventArgs e)
+        {
+            infoStatusLabel.Text = "Обучить нейросеть с указанными параметрами";
+        }
+
+        private void testNetButton_MouseEnter(object sender, EventArgs e)
+        {
+            infoStatusLabel.Text = "Тестировать нейросеть на тестовой выборке такого же размера";
+        }
+
+        private void btnCamera_Click(object sender, EventArgs e)
+        {
+            var form = new Camera(Net, dataset);
+            form.Show();
+        }
+    }
+}
